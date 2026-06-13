6a8f3f77d9f108634a063c19120c53a140a36f80
Varchas Jasti <jastivarchas@gmail.com>
2026-06-14 01:13:47 +0530

    add User.js

---
+++ backend/src/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    hashed_password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Before saving a user, hash their password if it was changed
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("hashed_password")) return next();
+  this.hashed_password = await bcrypt.hash(this.hashed_password, 10);
+  next();
+});
+
+// Method to check if an entered password matches the stored hash
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.hashed_password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
