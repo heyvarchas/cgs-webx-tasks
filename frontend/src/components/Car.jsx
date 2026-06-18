fe11085933ca7c648fdd075ba991341511f91687
Varchas Jasti <jastivarchas@gmail.com>
2026-06-18 19:15:10 +0530

    modify Car.jsx

--- frontend/src/components/Car.jsx
+++ frontend/src/components/Car.jsx
@@ -1,7 +1,7 @@
 import { useGLTF } from '@react-three/drei'
 
 export default function Car(props) {
-  const { scene } = useGLTF('/models/car.glb')
+  const { scene } = useGLTF('/models/car-v1.glb')
 
   return <primitive object={scene} {...props} />
 }
